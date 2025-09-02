5e299723149f020dba0fb7b1e3151efa1ce6de1c
shakil hossain <sh9743441@gmail.com>
2025-09-02 10:57:55 +0600

    message role

--- packages/client/src/components/Chatbot.tsx
+++ packages/client/src/components/Chatbot.tsx
@@ -12,13 +12,18 @@ type ChatResponse = {
   message: string;
 };
 
+type Message = {
+  content: string;
+  role: 'user' | 'bot';
+};
+
 const Chatbot = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const conversationId = useRef(crypto.randomUUID());
   const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
   const onSubmit = async ({ prompt }: FormData) => {
-    setMessages((prev) => [...prev, prompt]);
+    setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
 
     reset();
 
@@ -27,7 +32,7 @@ const Chatbot = () => {
       conversationId: conversationId.current,
     });
 
-    setMessages((prev) => [...prev, data.message]);
+    setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
@@ -39,9 +44,18 @@ const Chatbot = () => {
 
   return (
     <div>
-      <div>
+      <div className="flex flex-col gap-3 mb-10">
         {messages.map((message, index) => (
-          <p key={index}>{message}</p>
+          <p
+            key={index}
+            className={`px-3 py-1 rounded-xl ${
+              message.role === 'user'
+                ? 'bg-blue-600 text-white self-end'
+                : 'bg-gray-100 text-black self-start'
+            }`}
+          >
+            {message.content}
+          </p>
         ))}
       </div>
       <form
