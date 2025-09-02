97f1925e60381f1dc641998deb524728d3555226
shakil hossain <sh9743441@gmail.com>
2025-09-02 12:21:23 +0600

    feat: improve error handling in Chatbot component and display error messages

--- packages/client/src/components/Chatbot.tsx
+++ packages/client/src/components/Chatbot.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { set, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { FaArrowUp } from 'react-icons/fa';
 import ReactMarkDown from 'react-markdown';
@@ -21,6 +21,7 @@ type Message = {
 const Chatbot = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isBotTyping, setIsBotTyping] = useState(false);
+  const [error, setError] = useState('');
   const conversationId = useRef(crypto.randomUUID());
   const lastMessageRef = useRef<HTMLDivElement | null>(null);
   const { register, handleSubmit, reset, formState } = useForm<FormData>();
@@ -30,18 +31,25 @@ const Chatbot = () => {
   }, [messages]);
 
   const onSubmit = async ({ prompt }: FormData) => {
-    setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
-    setIsBotTyping(true);
+    try {
+      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+      setIsBotTyping(true);
+      setError('');
 
-    reset({ prompt: '' });
+      reset({ prompt: '' });
 
-    const { data } = await axios.post<ChatResponse>('api/chat', {
-      prompt,
-      conversationId: conversationId.current,
-    });
+      const { data } = await axios.post<ChatResponse>('api/chat', {
+        prompt,
+        conversationId: conversationId.current,
+      });
 
-    setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
-    setIsBotTyping(false);
+      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+    } catch (error) {
+      console.log(error);
+      setError('Something went wrong. Please try again.');
+    } finally {
+      setIsBotTyping(false);
+    }
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
@@ -82,6 +90,7 @@ const Chatbot = () => {
             <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]"></div>
           </div>
         )}
+        {error && <div className="text-red-500">{error}</div>}
       </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
