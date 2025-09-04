b0b559c7df9a8c83e3e46f0236fe6c2e95d0d583
shakil hossain <sh9743441@gmail.com>
2025-09-04 12:50:22 +0600

    feat: add dayjs dependency and implement review summary storage

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,13 +1,30 @@
+import dayjs from 'dayjs';
 import { PrismaClient, type Review } from '../generated/prisma';
+
+const prisma = new PrismaClient();
 
 export const reviewRepository = {
   async getReviews(productId: number, limit?: number): Promise<Review[]> {
-    const prisma = new PrismaClient();
-
     return prisma.review.findMany({
       where: { productId },
       orderBy: { created_at: 'desc' },
       take: limit,
+    });
+  },
+
+  storeReviewSummary(productId: number, summary: string) {
+    const expires_at = dayjs().add(7, 'days').toDate();
+
+    const data = {
+      content: summary,
+      expires_at,
+      productId,
+    };
+
+    return prisma.summary.upsert({
+      where: { productId },
+      create: data,
+      update: data,
     });
   },
 };
